9c63f234f1bbd79511d0c6f4a342736833e971dc
aurorex <selva_beat@hotmail.com>
2018-06-14 23:24:07 -0500

    modificando archivo conexion.php

--- conexion.php
+++ conexion.php
@@ -1,17 +1,18 @@
 <?php
 // conexion con la base de datos
-//$link = 'mysql:host=localhost;dbname=api';
-// $dbhost = getenv("MYSQL_SERVICE_HOST");
-// $dbname = getenv("databasename");
-// $dbuser = getenv("databaseuser");
-// $dbpwd = getenv("databasepassword");
+$dbhost = getenv("MYSQL_SERVICE_HOST");
+$dbname = getenv("databasename");
+$dbuser = getenv("databaseuser");
+$dbpwd = getenv("databasepassword");
+$link = "mysql:host=$dbhost;dbname=$dbname";
 
-$link = "mysql:host=localhost;dbname=reto_everis_db";
-$usuario = 'root';
-$pass = '';
+
+// $link = "mysql:host=localhost;dbname=reto_everis_db";
+// $usuario = 'root';
+// $pass = '';
 
 try {
-  $pdo = new PDO($link,$usuario,$pass);
+  $pdo = new PDO($link,$dbuser,$dbpwd);
   //echo 'conectado';
 }catch (PDOException $e) {
   print "¡Error!: " . $e->getMessage() . "<br/>";
